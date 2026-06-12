c8e36ffa9f60c0b14c3276a256310ac21b3fca76
horizon <iliasyunusov@yandex.ru>
2026-06-12 11:23:49 +0500

    feat(links): add link model and postgres repository

---
+++ internal/links/postgres/create_link.go
@@ -0,0 +1,55 @@
+package postgres
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/google/uuid"
+	core_errors "github.com/horizoonn/shortener/internal/errors"
+	"github.com/horizoonn/shortener/internal/links"
+	"github.com/horizoonn/shortener/internal/storage/postgres/pool"
+)
+
+func (r *Repository) CreateLink(ctx context.Context, link links.Link) (links.Link, error) {
+	ctx, cancel := context.WithTimeout(ctx, r.pool.OpTimeout())
+	defer cancel()
+
+	if link.ID == uuid.Nil {
+		return links.Link{}, fmt.Errorf("link id is empty: %w", core_errors.ErrInvalidArgument)
+	}
+	if link.Code == "" {
+		return links.Link{}, fmt.Errorf("link code is empty: %w", core_errors.ErrInvalidArgument)
+	}
+	if link.OriginalURL == "" {
+		return links.Link{}, fmt.Errorf("original URL is empty: %w", core_errors.ErrInvalidArgument)
+	}
+
+	query := `
+	INSERT INTO links (id, code, original_url, is_custom, created_at, disabled_at)
+	VALUES ($1, $2, $3, $4, $5, $6)
+	RETURNING id, code, original_url, is_custom, created_at, disabled_at;
+	`
+
+	row := r.pool.QueryRow(
+		ctx,
+		query,
+		link.ID,
+		link.Code,
+		link.OriginalURL,
+		link.IsCustom,
+		link.CreatedAt,
+		link.DisabledAt,
+	)
+
+	var linkModel LinkModel
+	if err := linkModel.Scan(row); err != nil {
+		if errors.Is(err, pool.ErrUniqueViolation) {
+			return links.Link{}, fmt.Errorf("link code %q already exists: %w", link.Code, core_errors.ErrConflict)
+		}
+
+		return links.Link{}, fmt.Errorf("scan created link: %w", err)
+	}
+
+	return modelToDomain(linkModel), nil
+}
